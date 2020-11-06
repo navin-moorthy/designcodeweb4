55258204ed4e6b7fe06f94f822402518c948597b
Navin <navin007.a@gmail.com>
2020-11-06 09:02:53 +0530

    chore(metadata): 🔍  improve site metadata for seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ function IndexPage() {
     <Layout>
       <SEO title="Home" />
       <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
+      <h1>Design and code React apps</h1>
       <p>Welcome to your new Gatsby site.</p>
       <Link to="/page-2/">Go to page 2</Link> <br />
     </Layout>
